233d21c5d4275ce534a0e94b2a21348e91190f77
RAJESH RANJAN <64878501+Rajesh42@users.noreply.github.com>
2020-05-31 12:58:27 +0530

    add initial commit

---
+++ towerOfHanoi.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+void toh(int n, int from, int to, int aux, long long &moves);
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        long long moves = 0;
+        int N;
+        cin >> N;
+        toh(N, 1, 3, 2, moves);
+        cout << moves << endl;
+    }
+    return 0;
+}
+void toh(int N, int from, int to, int aux, long long &moves) {
+    if(N==1){cout<<"move disk 1 from rod "<<from<<" to rod "<<to<<endl;moves++;return;}
+    toh(N-1,from,aux,to,moves);
+    {cout<<"move disk "<<N<<" from rod "<<from<<" to rod "<<to<<endl;moves++;}
+    toh(N-1,aux,to,from,moves);
+}
