7bfdc87053f766bd2e6e2c0e3bb306ff44cd520d
RAJESH RANJAN <64878501+Rajesh42@users.noreply.github.com>
2020-08-08 09:24:56 +0530

    Update casino.cpp
     currency issue resolved

--- Casino.cpp
+++ Casino.cpp
@@ -21,28 +21,29 @@ int main()
     cout << "\n\n   Enter Your Name : ";
     getline(cin, Player);
     do {
-        cout << "\n   Enter Amount greater than or equal to 100 ₹";
-        cout << "\n   Enter Deposit amount to play game : ₹ ";
+        cout << "\n   Enter Amount greater than or equal to Rs. 100";
+        cout << "\n   Enter Deposit amount to play game : Rs. ";
         cin >> amount;
     } while (amount < 100);
     do
     {
         system("cls");
         rules();
-        cout << "\n   Your current balance is ₹ " << amount << "\n";
+        cout << "\n   Your current balance is Rs. " << amount << "\n";
         do
         {
-            cout << Player << ", enter money to bet : ₹";
+            cout <<"   "<< Player << ", enter money to bet : Rs.";
             cin >> bettingAmount;
             if (bettingAmount > amount)
-                cout << "   Your betting amount " << bettingAmount << " is more than your current balance " << amount << "\n" << "\nRe-enter data\n ";
+                cout << "   Your betting amount " << bettingAmount << " is more than your current balance " << amount << "\n" 
+                <<"\n"<<"   Re-enter data\n ";
         } while (bettingAmount > amount);
         do
         {
             cout << "   Guess your number to bet between 1 to 10 :";
             cin >> guess;
             if (guess <= 0 || guess > 10)
-                cout << "   Please check the number || should be between 1 to 10\n" << "\nRe-enter data\n ";
+                cout << "   Please check the number || should be between 1 to 10\n" << "\n   Re-enter data\n";
         } while (guess <= 0 || guess > 10);
 
         dice = rand() % 10 + 1;
@@ -54,24 +55,24 @@ int main()
         }
         else
         {
-            cout << "   Bad Luck this time  || You lost $ " << bettingAmount << "\n";
+            cout << "   Bad Luck this time  || You lost Rs. " << bettingAmount << "\n";
             amount = amount - bettingAmount;
         }
 
         cout << "\n   The winning number was : " << dice << "\n";
-        cout << "\n" << Player << ", You have $ " << amount << "\n";
+        cout <<"   "<<Player << ", You have Rs. " << amount << "\n";
         if (amount == 0)
         {
             cout << "   You have no money to play ";
             break;
         }
-        cout << "\n\n   ---->Do you want to play again (y/n)? ";
+        cout << "\n\n   ---> Do you want to play again (y/n)? ";
         cin >> choice;
     } while (choice == 'Y' || choice == 'y');
 
     cout << "\n\n";
     drawLine(73, '*');
-    cout << "\n       Thanks for playing game. Your balance amount is $ " << amount << "\n\n";
+    cout << "\n   Thanks for playing game. Your balance amount is Rs. " << amount << "\n\n";
     drawLine(73, '*');
 
     return 0;
